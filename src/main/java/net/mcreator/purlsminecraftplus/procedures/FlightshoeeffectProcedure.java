aa0d5c3a08f2673362c451256a5d4ab66c73dc18
evanpurl <evan@purlcomputers.com>
2023-04-21 22:27:28 -0600

    don't remember

--- src/main/java/net/mcreator/purlsminecraftplus/procedures/FlightshoeeffectProcedure.java
+++ src/main/java/net/mcreator/purlsminecraftplus/procedures/FlightshoeeffectProcedure.java
@@ -5,11 +5,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.TickEvent;
 
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.client.Minecraft;
 
 import net.mcreator.purlsminecraftplus.init.PurlsMinecraftPlusModItems;
 
@@ -41,9 +44,21 @@ public class FlightshoeeffectProcedure {
 				_player.onUpdateAbilities();
 			}
 		} else {
-			if (entity instanceof Player _player) {
-				_player.getAbilities().mayfly = (false);
-				_player.onUpdateAbilities();
+			if (!(new Object() {
+				public boolean checkGamemode(Entity _ent) {
+					if (_ent instanceof ServerPlayer _serverPlayer) {
+						return _serverPlayer.gameMode.getGameModeForPlayer() == GameType.CREATIVE;
+					} else if (_ent.level.isClientSide() && _ent instanceof Player _player) {
+						return Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()) != null
+								&& Minecraft.getInstance().getConnection().getPlayerInfo(_player.getGameProfile().getId()).getGameMode() == GameType.CREATIVE;
+					}
+					return false;
+				}
+			}.checkGamemode(entity))) {
+				if (entity instanceof Player _player) {
+					_player.getAbilities().mayfly = (false);
+					_player.onUpdateAbilities();
+				}
 			}
 		}
 	}
